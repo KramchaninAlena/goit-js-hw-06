39d67db91490a4b17644b281301393616ce41c99
Alona <100025327+KramchaninAlena@users.noreply.github.com>
2023-06-27 19:45:05 +0300

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,13 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const itemEl = ingredients.map(ingredient => {
+const createEl = document.createElement('li')
+createEl.textContent = ingredient;
+createEl.classList.add('item');
+return createEl;
+});
+
+const listEl = document.querySelector('#ingredients');
+listEl.append(...itemEl);
