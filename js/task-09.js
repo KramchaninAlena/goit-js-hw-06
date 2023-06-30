d38b4ffd63f2cc9f90c1fbcbd8eb893f9ffdd3ba
Alona <100025327+KramchaninAlena@users.noreply.github.com>
2023-06-30 16:52:40 +0300

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -3,3 +3,16 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const buttonEl = document.querySelector('.change-color')
+const bodyEl = document.querySelector('body')
+const textEl = document.querySelector('.color')
+
+buttonEl.addEventListener('click',onButtonColor);
+
+function onButtonColor () {
+  bodyEl.style.backgroundColor = getRandomHexColor();
+  textEl.textContent = bodyEl.style.backgroundColor;
+}
+
+
