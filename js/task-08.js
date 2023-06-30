d2f303f4c907141229cb87b934b41628dd9a1897
Alona <100025327+KramchaninAlena@users.noreply.github.com>
2023-06-30 16:10:53 +0300

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const formEl = document.querySelector('.login-form');
 
+formEl.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit (event) {
+    event.preventDefault();
+    const {email, password} = event.currentTarget.elements;
+    
+if(password.value === '' || email.value === ''){
+   return alert ('Всі поля повинні бути заповнені')
+};
+
+const data = {
+        email: email.value,
+        password: password.value,
+    }
+    console.log(data);
+    formEl.reset();
+    
+};
